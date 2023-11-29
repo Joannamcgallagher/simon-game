36878304f7430e02b320a0221684f0ea87f5136f
Joannamcgallagher <>
2023-11-29 21:21:39 +0000

    Add newGam & showScore tests and functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,19 @@
 let game = {
     currentGame: [],
     score: 0,
-    playerMoves: [],
+    playerMoves: ["button1", "button2", "button3"],
     choices: ["button1", "button2", "button3", "button4"],
 };
 
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+}
 
-module.exports = { game };
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
